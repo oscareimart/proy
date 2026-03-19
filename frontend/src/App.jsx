79058ed6921ac3ef7a27762ff74f9ada193660eb
Oscar Eimar <oeticona@gmail.com>
2026-03-18 23:04:38 -0400

    feat: add SVG assets for React and Vite, implement Tabs component, and set up Tailwind CSS with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Tabs from "./components/ui/Tabs.jsx";
+
+function App() {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex w-5xl justify-center self-center">
+        <Tabs />
+      </div>
+    </div>
+  );
+}
+
+export default App;
